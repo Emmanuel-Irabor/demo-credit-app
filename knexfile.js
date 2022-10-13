d52be5636deca12540c854ec753f57909646bffe
Emmanuel Irabor <75018175+Emmywhyte93@users.noreply.github.com>
2022-10-13 02:05:41 +0100

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,11 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: process.env.MYSQL_DEV_HOST,
-      user: process.env.MYSQL_DEV_USER,
-      port: process.env.MYSQL_DEV_PORT,
-      password: process.env.MYSQL_DEV_PASSWORD,
-      database: process.env.MYSQL_DEV_DATABASE
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
     },
     migrations: {
       directory: "./database"
