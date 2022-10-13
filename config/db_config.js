ed2cbeee2f3a373becec023fc3e82705707522bf
Emmywhyte93 <emmanuel4sure93@gmail.com>
2022-10-13 03:13:48 +0100

    modified:   config/db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -2,7 +2,6 @@ const knex = require("knex")
 const knexConfig = require("../knexfile")
 require("dotenv").config();
 
-const environment = process.env.DB_ENV || "production"
-//const environment = process.env.DEV_ENV || process.env.NODE_ENV;
+const environment = process.env.DEV_ENV || process.env.NODE_ENV;
 
 module.exports = knex(knexConfig[environment])
